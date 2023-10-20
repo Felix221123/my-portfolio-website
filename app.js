45fe6dd54aa31c9f1ab13d9c8d08fcafea98b282
Felix Baah <felixbaah47@gmail.com>
2023-10-20 20:06:15 +0100

    made a navbar on mobile

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ let listOfNavs = document.querySelector('#my-list-of-headers');
 let anchorTagsElements = document.querySelectorAll('#my-list-of-headers li a');
 let portFolio = document.querySelector('.profile-name')
 let homeSection = document.querySelector('section#Home')
-
+let closeBtn = document.querySelector('#close-tbn');
+let navBar = document.querySelector('#navbar');
+let listItems = document.querySelectorAll('#my-list-of-headers li')
+let menuBar = document.querySelector('.fa-bars')
 
 listOfNavs.addEventListener('click', function (e) {
     const target = e.target
@@ -29,5 +32,42 @@ portFolio.addEventListener('click', function (e) {
     homeSection.scrollIntoView({behavior : 'smooth'})
     
 })
+
+//close button for menu to disappear
+closeBtn.addEventListener('click', function () {
+    
+    // navBar.style.animation = 'hide-menu 0.5s ease-in-out';
+    navBar.style.display = 'none'
+})
+
+
+
+menuBar.addEventListener('click', function () {
+    navBar.style.display = 'block';
+
+
+})
+
+listItems.forEach((element) => {
+    element.addEventListener('click', function (e) {
+        
+        //this allow applys for when the screen size is less than 768px
+        if (window.innerWidth <= 768) {
+            navBar.style.display = 'none';
+        }
+    })
+})
+
+//WITH THIS CODE , IT TEST WHETHER THE ELEMENT THAT TRIGGERES 
+//THE EVENT IS EQUAL TO THE ELEMENT AND IT APPEARS, IT HIDES A CONTAINER, EFFECTIVELY
+
+window.addEventListener('click', function (e) {
+    //whenever the user clicks anywhere on the screen apart from the navbar,
+    //it should hide the navbar effectively
+    if (e.target === navBar) {
+        navBar.style.display = 'none'
+    }
+})
+
 
 
