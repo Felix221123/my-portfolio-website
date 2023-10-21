44b9ce36bdc0207bc4fc33d553f982848f13db9f
Felix Baah <felixbaah47@gmail.com>
2023-10-21 14:38:15 +0100

    fixed navbar menu on mobile screen

--- app.js
+++ app.js
@@ -36,17 +36,18 @@ portFolio.addEventListener('click', function (e) {
 //close button for menu to disappear
 closeBtn.addEventListener('click', function () {
     
-    // navBar.style.animation = 'hide-menu 0.5s ease-in-out';
+    navBar.style.animation = 'hide-menu 0.5ms ease-in-out';
     navBar.style.display = 'none'
 })
 
 
 
 menuBar.addEventListener('click', function () {
+    navBar.style.animation = 'animate-menu ease-in-out 0.5s'
     navBar.style.display = 'block';
 
-
 })
+
 
 listItems.forEach((element) => {
     element.addEventListener('click', function (e) {
@@ -58,9 +59,10 @@ listItems.forEach((element) => {
     })
 })
 
+
+
 //WITH THIS CODE , IT TEST WHETHER THE ELEMENT THAT TRIGGERES 
 //THE EVENT IS EQUAL TO THE ELEMENT AND IT APPEARS, IT HIDES A CONTAINER, EFFECTIVELY
-
 window.addEventListener('click', function (e) {
     //whenever the user clicks anywhere on the screen apart from the navbar,
     //it should hide the navbar effectively
